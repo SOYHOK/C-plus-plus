05632e9fa60385831cbcbcfeb374958158dfff1f
SOYHOK <100107761+SOYHOK@users.noreply.github.com>
2022-08-09 08:19:59 +0700

    Create Example01.cpp

---
+++ Example01.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int x;
+  float y;
+  
+  cout << "Enter two numbers seperated by space: ";
+  cin >> x >> y;
+  cout << "You entered " << x << " and "<< y;
+  return 0;
+}
