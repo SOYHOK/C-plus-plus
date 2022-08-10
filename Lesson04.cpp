734248f6825024a200007deb71a5cc57de568246
SOYHOK <100107761+SOYHOK@users.noreply.github.com>
2022-08-10 15:11:25 +0700

    Create Lesson04.cpp

---
+++ Lesson04.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+  bool b1= true;// declaring a booleanvariable with true value 
+  bool b2= false;// declaring a boolean variable with true value 
+  cout << b1 << endl; 
+  cout << b2;
+  return 0; 
+  
+}
